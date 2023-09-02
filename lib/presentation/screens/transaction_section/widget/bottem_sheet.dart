616c516f940aeecbc81fed77e2943abe077a8949
Suhail <mspk9645@gmail.com>
2023-09-02 10:58:20 +0530

    error fixed

--- lib/presentation/screens/transaction_section/widget/bottem_sheet.dart
+++ lib/presentation/screens/transaction_section/widget/bottem_sheet.dart
@@ -72,157 +72,11 @@ class FilterBottermSheet extends StatelessWidget {
                       ],
                     ),
                     spaceForheight10,
-                    Container(
-                      padding: EdgeInsets.all(8),
-                      height: 85,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
-                            'Money in and out · 2',
-                            style: GoogleFonts.notoSans(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          BlocBuilder<MoneyFilter, int>(
-                            builder: (context, state) {
-                              return Row(
-                                children: List.generate(
-                                  2,
-                                  (index) => InkWell(
-                                    onTap: () {
-                                      BlocProvider.of<MoneyFilter>(context)
-                                          .onTap(index);
-                                    },
-                                    child: Container(
-                                      margin: const EdgeInsets.only(right: 10),
-                                      decoration: BoxDecoration(
-                                          color: index == state
-                                              ? const Color.fromARGB(
-                                                  255, 208, 236, 245)
-                                              : Colors.transparent,
-                                          borderRadius:
-                                              BorderRadius.circular(10)),
-                                      child: Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 10, vertical: 4),
-                                        child: Text(
-                                          filterMoneyInAndOutOptions[index],
-                                          style: GoogleFonts.notoSans(
-                                              fontSize: 14,
-                                              color: index == state
-                                                  ? const Color(0xFF0CABDF)
-                                                  : const Color(0xFF75808A)),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          )
-                        ],
-                      ),
-                    ),
+                 const FilterMoney(),
                     spaceForheight20,
                     BlocBuilder<StatusFilter, int>(
                       builder: (context, state) {
-                        return Container(
-                          padding: EdgeInsets.all(8),
-                          height: 120,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text(
-                                'Statuses · 6',
-                                style: GoogleFonts.notoSans(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Row(
-                                children: List.generate(
-                                  3,
-                                  (index) => InkWell(
-                                    onTap: () {
-                                      BlocProvider.of<StatusFilter>(context)
-                                          .onTap(index);
-                                    },
-                                    child: Container(
-                                      margin: const EdgeInsets.only(right: 10),
-                                      decoration: BoxDecoration(
-                                          color: index == state
-                                              ? const Color.fromARGB(
-                                                  255, 208, 236, 245)
-                                              : Colors.transparent,
-                                          borderRadius:
-                                              BorderRadius.circular(10)),
-                                      child: Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 10, vertical: 4),
-                                        child: Text(
-                                          filterStatuses[index],
-                                          style: GoogleFonts.notoSans(
-                                              fontSize: 14,
-                                              color: index == state
-                                                  ? const Color(0xFF0CABDF)
-                                                  : const Color(0xFF75808A)),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Row(
-                                children: List.generate(
-                                  3,
-                                  (index) => InkWell(
-                                    onTap: () {
-                                      BlocProvider.of<StatusFilter>(context)
-                                          .onTap(index + 3);
-                                    },
-                                    child: Container(
-                                      margin: const EdgeInsets.only(right: 10),
-                                      decoration: BoxDecoration(
-                                          color: index + 3 == state
-                                              ? const Color.fromARGB(
-                                                  255, 208, 236, 245)
-                                              : Colors.transparent,
-                                          borderRadius:
-                                              BorderRadius.circular(10)),
-                                      child: Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 10, vertical: 4),
-                                        child: Text(
-                                          filterStatuses[index + 3],
-                                          style: GoogleFonts.notoSans(
-                                            fontSize: 14,
-                                            color: index + 3 == state
-                                                ? const Color(0xFF0CABDF)
-                                                : const Color(0xFF75808A),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        );
+                        return FilterStatus(state: state,);
                       },
                     ),
                     spaceForheight20,
@@ -489,6 +343,174 @@ class FilterBottermSheet extends StatelessWidget {
               ),
             ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class FilterStatus extends StatelessWidget {
+  const FilterStatus({
+    super.key, required this.state,
+  });
+final int state;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      height: 120,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'Statuses · 6',
+            style: GoogleFonts.notoSans(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Row(
+            children: List.generate(
+              3,
+              (index) => InkWell(
+                onTap: () {
+                  BlocProvider.of<StatusFilter>(context)
+                      .onTap(index);
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  decoration: BoxDecoration(
+                      color: index == state
+                          ? const Color.fromARGB(
+                              255, 208, 236, 245)
+                          : Colors.transparent,
+                      borderRadius:
+                          BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 4),
+                    child: Text(
+                      filterStatuses[index],
+                      style: GoogleFonts.notoSans(
+                          fontSize: 14,
+                          color: index == state
+                              ? const Color(0xFF0CABDF)
+                              : const Color(0xFF75808A)),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: List.generate(
+              3,
+              (index) => InkWell(
+                onTap: () {
+                  BlocProvider.of<StatusFilter>(context)
+                      .onTap(index + 3);
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  decoration: BoxDecoration(
+                      color: index + 3 == state
+                          ? const Color.fromARGB(
+                              255, 208, 236, 245)
+                          : Colors.transparent,
+                      borderRadius:
+                          BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 4),
+                    child: Text(
+                      filterStatuses[index + 3],
+                      style: GoogleFonts.notoSans(
+                        fontSize: 14,
+                        color: index + 3 == state
+                            ? const Color(0xFF0CABDF)
+                            : const Color(0xFF75808A),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class FilterMoney extends StatelessWidget {
+  const FilterMoney({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      height: 85,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'Money in and out · 2',
+            style: GoogleFonts.notoSans(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          BlocBuilder<MoneyFilter, int>(
+            builder: (context, state) {
+              return Row(
+                children: List.generate(
+                  2,
+                  (index) => InkWell(
+                    onTap: () {
+                      BlocProvider.of<MoneyFilter>(context)
+                          .onTap(index);
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      decoration: BoxDecoration(
+                          color: index == state
+                              ? const Color.fromARGB(
+                                  255, 208, 236, 245)
+                              : Colors.transparent,
+                          borderRadius:
+                              BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 4),
+                        child: Text(
+                          filterMoneyInAndOutOptions[index],
+                          style: GoogleFonts.notoSans(
+                              fontSize: 14,
+                              color: index == state
+                                  ? const Color(0xFF0CABDF)
+                                  : const Color(0xFF75808A)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          )
         ],
       ),
     );
